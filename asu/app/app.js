28c1361259a102e9a7258991deebdcd84dbef24b
sonoftaraz <wewensign@rootfest.net>
2016-11-16 09:36:06 +0600

    Update app.js

--- asu/app/app.js
+++ asu/app/app.js
@@ -27,6 +27,6 @@ blogApp.controller('PostsController', ['$scope', '$http' ,function($scope, $http
 		
 	$http.get('data/posts.json').success(function(data){
 	  $scope.posts = data;
-	});﻿
+	});
 
 }]);
